245fff06e326a09e686856e5bef4ef8d5659bc02
26blevine <26blevine@gannacademy.org>
2025-09-03 13:26:18 -0400

    thesaurus who?

--- src/times_ten.py
+++ src/times_ten.py
@@ -1,6 +1,9 @@
 # Write your solution here
 def times_ten(start_index: int, end_index: int):
-    pass
+    vals = {}
+    for i in range (start_index, end_index+1):
+        vals[i] = 10*i
+    return vals
 
 
 if __name__ == "__main__":
